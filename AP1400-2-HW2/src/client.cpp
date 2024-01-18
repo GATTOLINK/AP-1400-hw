0551f6c3209d924a9f0abff734094892c5647024
GATTOLINK <2640537189@qq.com>
2024-01-18 01:28:06 -0800

    Author: GATTOLINK
    Date:
    2024/01/18
    
    Type:[bugfix, feature, refactor, docs, style, test, chore]
    test
    
    Description:
    AP1400-HW2 function test

--- AP1400-2-HW2/src/client.cpp
+++ AP1400-2-HW2/src/client.cpp
@@ -1,1 +1,29 @@
 #include "client.h"
+
+Client::Client(std::string id, const Server& server) : id(id), server(&server) {
+    crypto::generate_key(this->public_key, this->private_key);
+}
+
+std::string Client::get_id() {
+    return this->id;
+}
+
+std::string Client::get_publickey() {
+    return this->public_key;
+}
+
+double Client::get_wallet() {
+    
+}
+
+std::string Client::sign(std::string txt) {
+
+}
+
+bool Client::transfer_money(std::string receiver, double value) {
+
+}
+
+size_t generate_nonce() {
+    
+}
--- AP1400-2-HW2/src/server.cpp
+++ AP1400-2-HW2/src/server.cpp
@@ -1,1 +1,29 @@
 #include "server.h"
+
+std::vector<std::string> pending_trxs;
+
+Server::Server() { }
+
+std::shared_ptr<Client> Server::add_client(std::string id) {
+
+}
+
+std::shared_ptr<Client> Server::get_client(std::string id) {
+
+}
+
+double Server::get_wallet(std::string id) {
+
+}
+
+bool Server::parse_trx(const std::string trx, std::string sender, std::string receiver, double value) {
+
+}
+
+bool Server::add_pending_trx(std::string trx, std::string signature) {
+
+}
+
+size_t Server::mine() {
+
+}
